efbfbf50ce086e3dda0d33bad5fa5b816211ebb8
Francisco <gabrinelson27@gmail.com>
2018-09-29 14:21:40 -0300

    added a defaultnlpprovider

---
+++ BuilderInterpreter/Services/DefaultNlpProvider.cs
@@ -0,0 +1,11 @@
+﻿using BuilderInterpreter.Interfaces;
+using BuilderInterpreter.Models;
+using System.Threading.Tasks;
+
+namespace BuilderInterpreter.Services
+{
+    internal class DefaultNlpProvider : INlpProvider
+    {
+        public Task<NlpResponse> GetNlpResponse(string input, string userIdentity) => Task.FromResult<NlpResponse>(null);
+    }
+}
